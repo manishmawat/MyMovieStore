4ddb3b80cca1d0aad7376372a4827f225766672b
Manish Kumar Mawatwal <manmawat@publicisgroupe.net>
2018-07-22 21:15:30 -0400

    Add project files.

---
+++ MyMovieStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMovieStore.Startup))]
+namespace MyMovieStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
